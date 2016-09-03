c2c01ce82fad74f93641c3666c73675c77348271
Matt Long <matt.long@matthew-long.com>
2016-09-02 20:03:28 -0600

    Initial code commit

--- ShowDownload/ViewController.swift
+++ ShowDownload/ViewController.swift
@@ -10,16 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var webView:UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let url = URL(string: "https://bitcoin.org/bitcoin.pdf")
+        
+        let task = URLSession.shared.downloadTask(with: url!) { (localUrl, response, error) in
+            // Have to copy the downloaded file somewhere else or it will get purged
+            let filename = "outputfilename.pdf"
+            let outputUrl = FileManager.default.documentsDirectoryURL.appendingPathComponent(filename)
+            do {
+                // The file is now in our documents directory
+                try FileManager.default.copyItem(at: localUrl!, to: outputUrl)
+            } catch {
+                
+            }
+
+            // Call back out to the main thread to load the resource
+            DispatchQueue.main.async {
+                self.webView.loadRequest(URLRequest(url: outputUrl))
+            }
+        }
+        
+        task.resume()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
+// Add an extension to the file manager to simplify grabbing the documents directory
+extension FileManager {
+    var documentsDirectoryURL : URL {
+        get {
+            let urls = self.urls(for: .documentDirectory, in: .userDomainMask)
+            return urls[urls.endIndex-1]
+        }
+    }
+}
